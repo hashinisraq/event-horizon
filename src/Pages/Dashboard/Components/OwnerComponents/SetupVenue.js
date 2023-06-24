97f0a8f02e6be340cf9226b4ea9d8267bf863d95
hashinisraq <hashinisraq.hi@gmail.com>
2023-06-25 04:07:40 +0600

    modified

--- src/Pages/Dashboard/Components/OwnerComponents/SetupVenue.js
+++ src/Pages/Dashboard/Components/OwnerComponents/SetupVenue.js
@@ -69,6 +69,7 @@ const SetupVenue = () => {
                 .then(result => {
                 })
             setProvideData({});
+            setAvailability([{ startTime: '', endTime: '' }]);
         }
         e.preventDefault();
     }
